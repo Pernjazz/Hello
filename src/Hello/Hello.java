06ed8a8dd0b18b4545d5fa84cf14451b6130abb2
Pernjazz <rongtaofongnum@hotmail.com>
2014-10-11 18:26:19 +0700

    Update Hello.java

--- src/Hello/Hello.java
+++ src/Hello/Hello.java
@@ -7,7 +7,7 @@ public class Hello {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("ฉันชื่อสุพัตรา");
+System.out.println("Hello GitHub");
 	}
 
 }
